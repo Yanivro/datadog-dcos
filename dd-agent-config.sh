dfc0695d7ae82c822b1fc06f56d03c32aab78f47
Yaniv.Rozenblat <yaniv.rozenblat@biocatch.com>
2017-02-07 18:45:07 +0200

    initial commit

--- dd-agent-config.sh
+++ dd-agent-config.sh
@@ -2,6 +2,7 @@
 
 DD_API_KEY=$1
 ENV_TAG=$2
+HOST_IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 
 ## Set up apt so that it can download through https ##
 until sudo apt-get -y update && sudo apt-get -y install apt-transport-https
@@ -32,7 +33,7 @@ sudo cp /etc/dd-agent/conf.d/mesos_slave.yaml.example /etc/dd-agent/conf.d/mesos
 sudo sed -i "s/# docker_root:/docker_root/g" /etc/dd-agent/conf.d/docker_daemon.yaml
 sudo sed -i 's/# collect_labels_as_tags:/collect_labels_as_tags:/g' /etc/dd-agent/conf.d/docker_daemon.yaml
 sudo sed -i 's/"com.docker.compose.service", "com.docker.compose.project"/"customer_name"/g' /etc/dd-agent/conf.d/docker_daemon.yaml
-sudo sed -i "s/localhost/$HOST/g" /etc/dd-agent/conf.d/mesos_slave.yaml
+sudo sed -i "s/localhost/$HOST_IP/g" /etc/dd-agent/conf.d/mesos_slave.yaml
 sudo sed -i "s/# tags: mytag, env:prod, role:database/ tags: env:$ENV_TAG, role:mesos-slave/g" /etc/dd-agent/datadog.conf
 
 ## Start the Agent ##
