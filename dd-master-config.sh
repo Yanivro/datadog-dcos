f774064def78c730a2173e3feae764acf50754ae
Yaniv Rozenblat <spike77707@gmail.com>
2018-03-05 20:17:27 +0200

    Update dd-master-config.sh

--- dd-master-config.sh
+++ dd-master-config.sh
@@ -16,15 +16,15 @@ do
 done
 
 ## Set up the Datadog deb repo on your system and import Datadog's apt key ##
-sudo sh -c "echo 'deb https://apt.datadoghq.com/ stable main' > /etc/apt/sources.list.d/datadog.list"
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C7A7DA52
+sudo sh -c "echo 'deb https://apt.datadoghq.com/ stable 6' > /etc/apt/sources.list.d/datadog.list"
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 382E94DE
 
 ## Update your local apt repo and install the Agent ##
 sudo apt-get -y update
 sudo apt-get -y install datadog-agent
 
 ## Copy the example config into place and plug in your API key  ##
-sudo sh -c "sed 's/api_key:.*/api_key: $DD_API_KEY/' /etc/dd-agent/datadog.conf.example > /etc/dd-agent/datadog.conf"
+sudo sh -c "sed 's/api_key:.*/api_key: $DD_API_KEY/' /etc/datadog-agent/datadog.yaml.example > /etc/datadog-agent/datadog.yaml"
 
 ## Create yaml files from examples
 sudo cp /etc/dd-agent/conf.d/mesos_master.yaml.example /etc/dd-agent/conf.d/mesos_master.yaml
@@ -36,17 +36,16 @@ sudo cp /etc/dd-agent/conf.d/marathon.yaml.example /etc/dd-agent/conf.d/marathon
 # sudo  sed -i "s/# hostname: mymachine.mydomain/hostname:$(hostname)/g" /etc/dd-agent/datadog.conf
 sudo sed -i "s/- host: localhost/- host: leader.mesos/g" /etc/dd-agent/conf.d/zk.yaml
 sudo sed -i "s/localhost/leader.mesos/g" /etc/dd-agent/conf.d/mesos_master.yaml
-sudo sed -i "s/# tags: mytag, env:prod, role:database/ tags: env:$ENV_TAG, role:mesos-master/g" /etc/dd-agent/datadog.conf
+sudo sed -i "s/# tags:/ tags: env:$ENV_TAG, role:mesos-master/g" /etc/datadog-agent/datadog.yaml
 sudo sed -i 's/# - url: "https:/- url: "http:/g' /etc/dd-agent/conf.d/marathon.yaml
 sudo sed -i "s/server:port/leader.mesos:8080/g" /etc/dd-agent/conf.d/marathon.yaml
 
 ## Enable local traffic to agent ##
-sudo sed -i.back 's,# non_local_traffic: no,non_local_traffic: true,' /etc/dd-agent/datadog.conf
-
+sudo sed -i.back 's,# dogstatsd_non_local_traffic: no,dogstatsd_non_local_traffic: true,' /etc/datadog-agent/datadog.yaml
 
 
 ## Start the Agent ##
- sudo /etc/init.d/datadog-agent start
+sudo systemctl restart datadog-agent.service
 
 ## Add our SSL certificates ##
 cd /opt/mesosphere/packages/adminrouter-*/nginx/conf/common/
